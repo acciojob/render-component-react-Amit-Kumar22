95de6960f2c7edc44a6c959527c1c51f19c907d6
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-12-07 11:33:17 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div>
+    <h1>Welcome to React!</h1>
+    <p>I am learning React. My life is getting better.</p>
+  </div>
   )
 }
-
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
